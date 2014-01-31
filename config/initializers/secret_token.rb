efc9f3a4140b0dbbde87efe2e05eb00ee6001fe6
atryom <atryomb@gmail.com>
2014-01-31 22:33:10 +0400

    taskcheck

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taskcheck::Application.config.secret_key_base = 'a3ac1662fd2a9cd870ae5888002cb81421a5c5bb781e792e244022e167a885feb63c0617738bdcd1e556f15cc6adcd761876b0ca09f06cbbb12521824f8c07ec'
