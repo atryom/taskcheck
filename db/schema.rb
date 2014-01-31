945b6cb3845c6012d2b55c5102493d0cb454649b
atryom <atryomb@gmail.com>
2014-01-31 23:08:41 +0400

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140131173221) do
+ActiveRecord::Schema.define(version: 20140131184902) do
+
+  create_table "statuses", force: true do |t|
+    t.string   "name"
+    t.integer  "order_num"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
