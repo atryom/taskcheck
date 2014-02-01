5d3f8ad2279a97250a3822f98ba7951b7db8c1f0
atryom <atryomb@gmail.com>
2014-02-02 02:03:06 +0400

    Time to Sleep

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Taskcheck::Application.routes.draw do
   devise_for :users
   resources :users
   root :to => 'welcome#index'
-  devise_for :admins, :controllers => { :sessions => "admins/sessions" }
+  #devise_for :admins, :controllers => { :sessions => "admins/sessions" }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
